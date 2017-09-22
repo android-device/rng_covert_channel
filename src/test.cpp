414d17bb417f0b34290fb285c3c5c3c7a588c092
Adam Auten <auty1395@gmail.com>
2017-09-22 14:38:44 -0500

    added stock code and some rdrand tests

---
+++ src/test.cpp
@@ -0,0 +1,34 @@
+
+#include"util.hpp"
+
+int main(int argc, char **argv)
+{
+	// Put your covert channel setup code here
+
+    CYCLES test[100];
+	printf("Please press enter.\n");
+
+	char text_buf[2];
+	fgets(text_buf, sizeof(text_buf), stdin);
+
+	printf("Receiver now listening.\n");
+
+	bool listening = true;
+	while (listening) {
+
+		// Put your covert channel code here
+        for(int i = 0; i < 40; i++)
+            test[i] = measure_one_rdseed_time();
+        for(int i = 0; i < 40; i++)
+            printf("iter %d: %d\n", i, test[i]);
+
+        printf("Please press enter.\n");
+        fgets(text_buf, sizeof(text_buf), stdin);
+	}
+
+	printf("Receiver finished.\n");
+
+	return 0;
+}
+
+
